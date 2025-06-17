d10f3c9aa063cea4703242390f0648100f78fef3
David Alexander Xiloj Marroquín <123334235+davoxan@users.noreply.github.com>
2025-06-17 16:42:21 -0600

    Add files via upload

--- 007.6_CODE.ino
+++ 007.6_CODE.ino
@@ -14,40 +14,40 @@ const int trigger_pin_1 = 7; // Pin del Arduino conectado al trigger del sensor 
 const int echo_pin_1 = 6; // Pin del Arduino conectado al echo del sensor ultrasónico #1
 const int clk = 8; // Pin del Arduino conectado al CLK del TM1637 principal
 const int dio = 9; // Pin del Arduino conectado al DIO del TM1637 principal
-const int trigger_pin_2 = 11; // Pin del Arduino conectado al trigger del sensor ultrasónico de la bandera
-const int echo_pin_2 = 10; // Pin del Arduino conectado al echo del sensor ultrasónico de la bandera
+const int trigger_pin_2 = 10; // Pin del Arduino conectado al trigger del sensor ultrasónico de la bandera
+const int echo_pin_2 = 11; // Pin del Arduino conectado al echo del sensor ultrasónico de la bandera
 const int trigger_pin_3 = 12; // Pin del Arduino conectado al trigger del sensor ultrasónico del suelo
 const int echo_pin_3 = 13; // Pin del Arduino conectado al echo del sensor ultrasónico del suelo
-const int next_button_pin = A0; //Botón para pasar al siguiente paso
-const int on_button_pin = A1; // Botón para encender el electroiman
-const int off_button_pin = A2; // Botón para apagar el electroiman
-const int one_two_button_pin = A3; // Botón para encender seleccionar "1" o "2"
+const int next_button_pin = A3; //Botón para pasar al siguiente paso
+const int on_button_pin = A2; // Botón para encender el electroiman
+const int off_button_pin = A1; // Botón para apagar el electroiman
+const int one_two_button_pin = A0; // Botón para encender seleccionar "1" o "2"
 
 // Variables de estado
 int relayState = 0;   // Estado inicial del relé
 int number_step = 1;
 int initial_step = 0;
 int anvil = 1;
-const int max_distance = 300; /// Distancia máxima que queremos medir en el ultrasonico #1 (en centímetros).
+const int max_distance = 280; /// Distancia máxima que queremos medir en el ultrasonico #1 (en centímetros).
 
-#define ALTURA_ACTIVACION 80 // Altura para activar el buzzer y brazo del hc sr04 del headform
+#define ALTURA_ACTIVACION 60 // Altura para activar el buzzer y brazo del hc sr04 del headform
 #define ALTURA_MAXIMA 2 //Altura máxima entre velocimetro y bandera
 
 bool buzzer_activated = false; // Variable para evitar que el buzzer se repita
-int initial_pos_laser = 0; //posición inicial del brazo laser
-int initial_pos_hc_head = 90; //posición inicial del brazo del HC-SR04 del headform
-int final_pos_laser = 45; //posición final del brazo laser, alineado al centro del anvil
-int final_pos_hc_head = 45; //posición final del brazo del HC-SR04 del headform, alineado al centro del anvil
+int initial_pos_laser = 25; //posición inicial del brazo laser
+int initial_pos_hc_head = 170; //posición inicial del brazo del HC-SR04 del headform
+int final_pos_laser = 122; //posición final del brazo laser, alineado al centro del anvil
+int final_pos_hc_head = 78; //posición final del brazo del HC-SR04 del headform, alineado al centro del anvil
 
-const int hc_flat = 2; //Distancia en cm del sensor ultrasonico del headform con la superficie del yunque plano
-const int hc_sphere = 3; //Distancia en cm del sensor ultrasonico del headform con la superficie del yunque semiesferico
-const int hc_2_ground = 0; //MODIFICAAAAAAAAAAAAAR  Distancia(cm) fija del ultrasonico de la bandera (medido desde su parte frontal) con el suelo
+const int hc_flat = 17; //Distancia en cm del sensor ultrasonico del headform con la superficie del yunque plano
+const int hc_sphere = 16; //Distancia en cm del sensor ultrasonico del headform con la superficie del yunque semiesferico
+const int hc_2_ground = 3; //Distancia(cm) fija del ultrasonico de la bandera (medido desde su parte frontal) con el suelo
 
 //Configuraciones iniciales de modulos
 LiquidCrystal_I2C lcd(0x27, 20, 4); // Dirección I2C 0x27, 20 columnas y 4 filas
 NewPing main_1(trigger_pin_1, echo_pin_1, max_distance); // Configuración del sensor ultrasónico #1
 NewPing flag_2(trigger_pin_2, echo_pin_2, max_distance); // Configuración del sensor ultrasónico BANDERA
-NewPing ground_3(trigger_pin_3, echo_pin_3, max_distance); // Configuración del sensor ultrasónico SUELO
+//NewPing ground_3(trigger_pin_3, echo_pin_3, max_distance); // Configuración del sensor ultrasónico SUELO
 TM1637Display display(clk, dio);
 Servo laser, hc_head;
 
@@ -57,7 +57,7 @@ unsigned long lastButtonPress_2 = 0; // Para controlar el tiempo de la última p
 unsigned long debounceDelay = 250;   // Tiempo para evitar el rebote (en milisegundos)
 
 // Definir el número de lecturas a promediar del ultrasónico
-#define NUM_READINGS 5 // Número de lecturas para promediar
+#define NUM_READINGS 2 // Número de lecturas para promediar
 
 // Variable para almacenar el último estado del botón
 int lastButtonState = LOW;  // El botón está inicialmente en bajo (no presionado)
@@ -68,6 +68,11 @@ bool stepActive = true;  // Puede ser 'true' cuando se quiera mostrar el paso, '
 
 bool countdown_active = false; // Estado de la cuenta regresiva
 
+int current_pos_laser = initial_pos_laser;  // variable global
+
+long dd = 0;  // ← variable global
+
+
 void setup() {
   //Asignación tipo de pin (lectura/escritura)
   pinMode(buzzer, OUTPUT);
@@ -77,6 +82,10 @@ void setup() {
   pinMode(off_button_pin, INPUT);
   pinMode(next_button_pin, INPUT);
   pinMode(one_two_button_pin, INPUT);
+  //pinMode(trigger_pin_2, OUTPUT); //pin como salida
+  //pinMode(echo_pin_2, INPUT);  //pin como entrada
+  //digitalWrite(trigger_pin_2, LOW);//Inicializamos el pin con 0
+
 
   //Inicialización modulos
   lcd.init(); // Initialize the LCD I2C display
@@ -90,7 +99,7 @@ void setup() {
   display.setBrightness(7); // Configurar el brillo de la pantalla del display TM1637
 
   //Inicialización del serial para seguir a tiempo real los datos a visualizar de interés
-  //Serial.begin(9600);
+  Serial.begin(9600);
 
   //escritura de variables al iniciar
   digitalWrite(relay_pin, relayState); // Asegurar que inicia en estado apagado
@@ -142,8 +151,10 @@ void loop() {
 
   digitalWrite(relay_pin, relayState);   // Aplicar el estado al relé
 
-  // Muestra el número de paso con el formato "P:1", "P:2", etc.
-  //showStep(number_step);
+if (stepActive && number_step != 4 && number_step != 9 && number_step != 10) {
+  showStepWithBlinkingUnderscore(number_step);
+}
+
 
 }
 
@@ -211,12 +222,12 @@ void update_electromagnet(){
 void lcd_setup(){
   lcd.setCursor(0, 0);            // move cursor the 1st row
   lcd.print("BIENVENIDO ESTIMADO"); 
-  lcd.setCursor(3, 1);            // move cursor to the 2nd row
-  lcd.print("USUARIO TITON A"); // print message at the 2nd row
-  lcd.setCursor(3, 2);            // move cursor to the 3rd row
-  lcd.print("LA MAQUINA DE"); // print message at the 3rd row
-  lcd.setCursor(1, 3);            // move cursor to the 4th row
-  lcd.print("IMPACTOS VERTICAL.");   // print message the 4th row
+  lcd.setCursor(2, 1);            // move cursor to the 2nd row
+  lcd.print("USUARIO TITON AL"); // print message at the 2nd row
+  lcd.setCursor(0, 2);            // move cursor to the 3rd row
+  lcd.print("IMPACTUN X, MAQUINA"); // print message at the 3rd row
+  lcd.setCursor(0, 3);            // move cursor to the 4th row
+  lcd.print("DE IMPACTOS VERTICAL");   // print message the 4th row
 }
 
 void step_2(){
@@ -232,27 +243,27 @@ void step_2(){
 }
 
 void step_3(){
-  lcd.setCursor(0, 0);            // move cursor the 1st row
-  lcd.print("'SIGUIENTE' SI ESTA"); 
+  lcd.setCursor(2, 0);            // move cursor the 1st row
+  lcd.print("'NEXT' SI ESTA"); 
   lcd.setCursor(1, 1);            // move cursor to the 2nd row
   lcd.print("SUJETO EL CARRITO."); // print message at the 2nd row
   lcd.setCursor(2, 2);            // move cursor to the 3rd row
   lcd.print("DE LO CONTRARIO,"); // print message at the 3rd row
-  lcd.setCursor(1, 3);            // move cursor to the 4th row
-  lcd.print("PRESIONAR 'AGARRE'");   // print message the 4th row
+  lcd.setCursor(3, 3);            // move cursor to the 4th row
+  lcd.print("PRESIONAR 'ON'");   // print message the 4th row
   update_electromagnet();
 }
 
 void step_4(){
+  flag_hcsr04();
   lcd.setCursor(0, 0);            // move cursor the 1st row
   lcd.print("SUBA EL CARRITO UNA"); 
-  lcd.setCursor(1, 1);            // move cursor to the 2nd row
-  lcd.print("ALTURA DE 1M. MIN."); // print message at the 2nd row
+  lcd.setCursor(2, 1);            // move cursor to the 2nd row
+  lcd.print("ALTURA DE 1M MIN."); // print message at the 2nd row
   lcd.setCursor(0, 2);            // move cursor to the 3rd row
-  lcd.print("ESCOJA ANILLO,ANVIL,"); // print message at the 3rd row
-  lcd.setCursor(2, 3);            // move cursor to the 4th row
-  lcd.print("HEADFORM Y CASCO");   // print message the 4th row
-  flag_hcsr04();
+  lcd.print("ESCOJA ANILLO, ANVIL,"); // print message at the 3rd row
+  lcd.setCursor(3, 3);            // move cursor to the 4th row
+  lcd.print("CABEZA Y CASCO");   // print message the 4th row
 }
 
 void step_5(){
@@ -277,13 +288,15 @@ void step_6(){
   lcd.print("IMPACTO EN EL CASCO"); // print message at the 3rd row
   lcd.setCursor(4, 3);            // move cursor to the 4th row
   lcd.print("PERTINENTE.");   // print message the 4th row
+  
   laser.write(final_pos_laser);
   digitalWrite(laser_pin, HIGH); 
 }
 
 void step_7(){
-  digitalWrite(laser_pin, LOW);
+
   laser.write(initial_pos_laser);
+  digitalWrite(laser_pin, LOW);
   lcd.setCursor(0, 0);            // move cursor the 1st row
   lcd.print("DESCIENDA EL CARRITO"); 
   lcd.setCursor(0, 1);            // move cursor to the 2nd row
@@ -303,7 +316,7 @@ void step_8(){
   lcd.print("VELOCIMETRO MENOR A"); // print message at the 3rd row
   lcd.setCursor(0, 3);            // move cursor to the 4th row
   lcd.print("2CM, SINO MODIFIQUE.");  // print message the 4th row
-  flag_velocimeter();
+  //flag_velocimeter();
 }
 
 void step_9(){
@@ -321,32 +334,33 @@ void step_9(){
 void step_10(){
   lcd.setCursor(1, 0);            // move cursor to the 1st row
   lcd.print("PRESIONE EL BOTON"); // print message at the 1st row
-  lcd.setCursor(1, 1);            // move cursor to the 2nd row
-  lcd.print("DE CONFIRMACION DE");   // print message at the 2nd row
+  lcd.setCursor(0, 1);            // move cursor to the 2nd row
+  lcd.print("DE 'ON' & 'OFF' PARA");   // print message at the 2nd row
   lcd.setCursor(0, 2);            // move cursor to the 3rd row
   lcd.print("SOLTADO, ALEJESE POR"); // print message at the 3rd row
   lcd.setCursor(1, 3);            // move cursor to the 4th row
   lcd.print("SEGURIDAD, GRACIAS");  // print message the 4th row
   int off_button = digitalRead(off_button_pin);
+  int on_button = digitalRead(on_button_pin);
 
   // Si se presiona el botón y la cuenta no ha comenzado
-  if (off_button == 1 && !countdown_active) {
+  if (off_button == 1 && on_button == 1 && !countdown_active) {
     delay(150); // Pequeño debounce
 
     hc_head.write(initial_pos_hc_head);
 
     countdown_active = true; // Activa la cuenta regresiva
 
-    // Cuenta regresiva de 10 a 0
-    for (int i = 10; i >= 0; i--) {
+    // Cuenta regresiva de 3 a 0
+    for (int i = 3; i >= 0; i--) {
     display.showNumberDec(i, true); // Muestra el número en el TM1637
 
     // Hacer sonar el buzzer una sola vez
-    digitalWrite(buzzer, HIGH);
+    /*digitalWrite(buzzer, HIGH);
     delay(100); // Bip corto
-    digitalWrite(buzzer, LOW);
+    digitalWrite(buzzer, LOW);*/
 
-    delay(900); // Espera el resto del segundo antes del siguiente número
+    delay(1000); // Espera el resto del segundo antes del siguiente número
   }
   
     countdown_active = false; // Resetea la variable para poder reiniciar
@@ -357,6 +371,16 @@ void step_10(){
 
 
 void main_hcsr04(){
+  long total_3 = 0;
+  // Tomar varias lecturas y sumarlas
+  for (int i = 0; i < NUM_READINGS; i++) {
+    total_3 += flag_2.ping_cm(); // Obtener la lectura actual
+    delay(50); // Pequeño retraso para la siguiente medición
+  }
+
+  // Calcular el promedio de las lecturas
+  long average_3 = total_3 / NUM_READINGS;
+
   long total = 0;
   // Tomar varias lecturas y sumarlas
   for (int i = 0; i < NUM_READINGS; i++) {
@@ -375,7 +399,7 @@ void main_hcsr04(){
   }
 
 
-  if(average > ALTURA_ACTIVACION){
+  if(average_3 < ALTURA_ACTIVACION){
     hc_head.write(initial_pos_hc_head);
   }
   else{
@@ -393,29 +417,33 @@ void flag_hcsr04(){
   }
 
   // Calcular el promedio de las lecturas
-  long average_2 = (total_2 / NUM_READINGS) + hc_2_ground;
+  long average_2 = total_2 / NUM_READINGS;
+
+  /*long tt; //timepo que demora en llegar el eco
+ 
+  digitalWrite(trigger_pin_2, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(trigger_pin_2, LOW);
+  
+  tt = pulseIn(echo_pin_2, HIGH); //obtenemos el ancho del pulso
+  dd = tt/59;             //escalamos el tiempo a una distancia en cm*/
+  
+  /*Serial.print("Distancia: ");
+  Serial.print(dd);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);*/
   
   // Mostrar el promedio en el display y en el monitor serial
   display.showNumberDec(average_2); // Mostrar en el display TM1637
 
-  // Si la distancia es 100 cm y el buzzer no ha sonado antes, lo activamos
-  if (average_2 >= ALTURA_ACTIVACION && average_2 <(ALTURA_ACTIVACION+10) && !buzzer_activated) {
-    digitalWrite(buzzer, HIGH);
-    delay(3000);  // Suena por 3 segundos
-    digitalWrite(buzzer, LOW);
-    buzzer_activated = true; // Marcamos que ya se activó
-  }
 
-  // Si la distancia cambia a otro valor diferente de 100 cm, reiniciamos la variable
-  if (average_2 < ALTURA_ACTIVACION || average_2 > (ALTURA_ACTIVACION+10)) {
-    buzzer_activated = false;
-  }
 }
 
-void flag_velocimeter(){
+/*void flag_velocimeter(){
   long total_3 = 0;
   // Tomar varias lecturas y sumarlas
-  for (int i = 0; i < NUM_READINGS; i++) {
+  /*for (int i = 0; i < NUM_READINGS; i++) {
     total_3 += flag_2.ping_cm(); // Obtener la lectura actual
     delay(50); // Pequeño retraso para la siguiente medición
   }
@@ -433,10 +461,10 @@ void flag_velocimeter(){
   // Mostrar el promedio en el display y en el monitor serial
   display.showNumberDec(average_3); // Mostrar en el display TM1637
 
-  // Si la distancia es 100 cm y el buzzer no ha sonado antes, lo activamos
+  // Si la distancia es 2 cm y el buzzer no ha sonado antes, lo activamos
   if (average_3 <= ALTURA_MAXIMA && average_3 >= 0 && !buzzer_activated) {
     digitalWrite(buzzer, HIGH);
-    delay(3000);  // Suena por 3 segundos
+    delay(50);  // Suena por 0.05 segundos
     digitalWrite(buzzer, LOW);
     buzzer_activated = true; // Marcamos que ya se activó
   }
@@ -445,57 +473,38 @@ void flag_velocimeter(){
   if (average_3 > ALTURA_MAXIMA || average_3 < 0) {
     buzzer_activated = false;
   }
-}
+}*/
 
-/* void showStep(int number_step) {
-  // Función para mostrar el paso en el formato "P:1", "P:2", etc.
+void showStepWithBlinkingUnderscore(int step) {
+  static bool underscoreVisible = true;
+  static unsigned long lastToggleTime = 0;
+  const unsigned long interval = 500; // tiempo de parpadeo (ms)
 
-  uint8_t data[] = {0x00, 0x00, 0x00, 0x00};  // Inicializa el array de datos para los segmentos
-
-  if (stepActive) {  // Solo muestra "P1", "P2", etc. cuando stepActive es verdadero
-    switch(number_step) {
-      case 1:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_B | SEG_C;  // '1'
-        break;
-      case 2:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_A | SEG_B | SEG_D | SEG_E | SEG_G;  // '2'
-        break;
-      case 3:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_A | SEG_B | SEG_G | SEG_D | SEG_C;  // '3'
-        break;
-      case 5:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_A | SEG_F | SEG_G | SEG_D | SEG_C;  // '5'
-        break;
-      case 6:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_A | SEG_F | SEG_G | SEG_D | SEG_C | SEG_E;  // '6'
-        break;
-      case 7:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_A | SEG_B | SEG_C ;  // '7'
-        break;
-      case 8:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G ;  // '8'
-        break;
-      case 11:
-        data[1] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;  // 'P'
-        data[2] = SEG_B | SEG_C ;  // '1'
-        data[3] = SEG_B | SEG_C ;  // '1'
-        break;
-
-    }
-  } else {
-    data[1] = 0x00;  // Apagar la posición 1
-    data[2] = 0x00;  // Apagar la posición 2
+  // Alternar visibilidad del guion bajo
+  if (millis() - lastToggleTime > interval) {
+    underscoreVisible = !underscoreVisible;
+    lastToggleTime = millis();
   }
 
-  // Muestra los segmentos en el display
+  uint8_t data[] = {0x00, 0x00, 0x00, 0x00};
+
+  // Letra 'P'
+  data[0] = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;
+
+  // Guion bajo (si está visible)
+  data[1] = underscoreVisible ? SEG_D : 0x00;
+
+  // Dígitos (los pasos deben ser menores a 100)
+  int tens = step / 10;
+  int units = step % 10;
+
+  if (step < 10) {
+    data[2] = display.encodeDigit(0);
+    data[3] = display.encodeDigit(units);
+  } else {
+    data[2] = display.encodeDigit(tens);
+    data[3] = display.encodeDigit(units);
+  }
+
   display.setSegments(data);
 }
-*/
-
